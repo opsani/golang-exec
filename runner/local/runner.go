62f0c1a47f348705c9357e0cdfd10997a0427730
Blake Watters <blakewatters@gmail.com>
2020-04-06 23:48:32 -0700

    Update script + runners to support the execution of interactive processes

--- runner/local/runner.go
+++ runner/local/runner.go
@@ -7,201 +7,213 @@
 package local
 
 import (
-    "context"
-    "errors"
-    "fmt"
-    "io"
-    "os/exec"
-    "strings"
-    "syscall"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
 
-    "github.com/stefaanc/golang-exec/script"
+	"github.com/stefaanc/golang-exec/script"
 )
 
 //------------------------------------------------------------------------------
 
 type Connection struct {
-    Type string   // must be "local"
+	Type string // must be "local"
 }
 
 type Error struct {
-    script   *script.Script
-    command  string
-    exitCode int
-    err      error
+	script   *script.Script
+	command  string
+	exitCode int
+	err      error
 }
 
 type Runner struct {
-    script  *script.Script
-    command string
-    cmd     *exec.Cmd
-    cancel  context.CancelFunc
+	script  *script.Script
+	command string
+	cmd     *exec.Cmd
+	cancel  context.CancelFunc
 
-    exitCode int
+	exitCode int
 }
 
 //------------------------------------------------------------------------------
 
-func (e *Error) Script()   *script.Script { return e.script }
-func (e *Error) Command()  string         { return e.command }
-func (e *Error) ExitCode() int            { return e.exitCode }
-func (e *Error) Error()    string         { return e.err.Error() }
-func (e *Error) Unwrap()   error          { return e.err }
+func (e *Error) Script() *script.Script { return e.script }
+func (e *Error) Command() string        { return e.command }
+func (e *Error) ExitCode() int          { return e.exitCode }
+func (e *Error) Error() string          { return e.err.Error() }
+func (e *Error) Unwrap() error          { return e.err }
 
 //------------------------------------------------------------------------------
 
 func New(connection interface{}, s *script.Script, arguments interface{}) (*Runner, error) {
-    if s.Error != nil {
-        return nil, &Error{
-            script: s,
-            exitCode: -1,
-            err: fmt.Errorf("[golang-exec/runner/local/New()] script failed to parse: %#w\n", s.Error),
-        }
-    }
+	if s.Error != nil {
+		return nil, &Error{
+			script:   s,
+			exitCode: -1,
+			err:      fmt.Errorf("[golang-exec/runner/local/New()] script failed to parse: %#w\n", s.Error),
+		}
+	}
 
-    r := new(Runner)
-    r.script = s
-    r.command = s.Command()
+	scriptReader, err := s.NewReader(arguments)
+	if err != nil {
+		return nil, &Error{
+			script:   s,
+			exitCode: -1,
+			err:      fmt.Errorf("[golang-exec/runner/local/New()] cannot create stdin reader: %#w\n", err),
+		}
+	}
+	var script string
+	if b, err := ioutil.ReadAll(scriptReader); err == nil {
+		script = string(b)
+	}
 
-    stdin, err := s.NewReader(arguments)
-    if err != nil {
-        return nil, &Error{
-            script: s,
-            exitCode: -1,
-            err: fmt.Errorf("[golang-exec/runner/local/New()] cannot create stdin reader: %#w\n", err),
-        }
-    }
+	r := new(Runner)
+	r.script = s
+	r.command = s.Command() + script
 
-    // create command, ready to start
-    ctx, cancel := context.WithCancel(context.Background())
-    args := strings.Split(r.command, " ")
-    var cmd *exec.Cmd
-    if args[0] == "cmd" {
-        // cmd has argument-escaping rules that are different from other programs, so needs different treatment
-        cmd = exec.CommandContext(ctx, args[0])
-        cmd.SysProcAttr = &syscall.SysProcAttr{
-            CmdLine: " " + strings.Join(args[1:], " "),
-        }
-    } else {
-        cmd = exec.CommandContext(ctx, args[0], args[1:]...)
-    }
-    r.cmd = cmd
-    r.cmd.Stdin  = stdin
-    r.cancel = cancel
+	// create command, ready to start
+	ctx, cancel := context.WithCancel(context.Background())
+	args := strings.SplitN(r.command, " ", 3)
+	var cmd *exec.Cmd
+	if args[0] == "cmd" {
+		// cmd has argument-escaping rules that are different from other programs, so needs different treatment
+		cmd = exec.CommandContext(ctx, args[0])
+		// cmd.SysProcAttr = &syscall.SysProcAttr{
+		// 	CmdLine: " " + strings.Join(args[1:], " "),
+		// }
+	} else {
+		fmt.Printf("The args are %v", args)
+		cmd = exec.CommandContext(ctx, args[0], args[1:]...)
+		// cmd = exec.CommandContext(ctx, "/bin/zsh", "-c", "docker run -ti -v ~/.kube:/root/.kube imb")
+	}
 
-    return r, nil
+	r.cmd = cmd
+	// r.cmd.Stdin = stdin
+	// stdinTTY := term.NewTTY(stdin)
+	// stdinTTY := term.NewTTY(os.Stdin)
+	// r.cmd.Stdin = stdinTTY
+	r.cmd.Stdin = os.Stdin
+	r.cancel = cancel
+
+	return r, nil
 }
 
 //------------------------------------------------------------------------------
 
 func (r *Runner) SetStdoutWriter(stdout io.Writer) {
-    r.cmd.Stdout = stdout
+	r.cmd.Stdout = stdout
 }
 
 func (r *Runner) SetStderrWriter(stderr io.Writer) {
-    r.cmd.Stderr = stderr
+	r.cmd.Stderr = stderr
 }
 
 func (r *Runner) StdoutPipe() (io.Reader, error) {
-    reader, err := r.cmd.StdoutPipe()
-    if err != nil {
-        r.exitCode = -1
-        return nil, &Error{
-            script: r.script,
-            exitCode: r.exitCode,
-            err: fmt.Errorf("[golang-exec/runner/local/StdoutPipe()] cannot create stdout reader: %#w\n", err),
-        }
-    }
+	reader, err := r.cmd.StdoutPipe()
+	if err != nil {
+		r.exitCode = -1
+		return nil, &Error{
+			script:   r.script,
+			exitCode: r.exitCode,
+			err:      fmt.Errorf("[golang-exec/runner/local/StdoutPipe()] cannot create stdout reader: %#w\n", err),
+		}
+	}
 
-    return reader, nil
+	return reader, nil
 }
 
 func (r *Runner) StderrPipe() (io.Reader, error) {
-    reader, err := r.cmd.StderrPipe()
-    if err != nil {
-        r.exitCode = -1
-        return nil, &Error{
-            script: r.script,
-            exitCode: r.exitCode,
-            err: fmt.Errorf("[golang-exec/runner/local/StderrPipe()] cannot create stderr reader: %#w\n", err),
-        }
-    }
+	reader, err := r.cmd.StderrPipe()
+	if err != nil {
+		r.exitCode = -1
+		return nil, &Error{
+			script:   r.script,
+			exitCode: r.exitCode,
+			err:      fmt.Errorf("[golang-exec/runner/local/StderrPipe()] cannot create stderr reader: %#w\n", err),
+		}
+	}
 
-    return reader, nil
+	return reader, nil
 }
 
 func (r *Runner) Run() error {
-    err := r.cmd.Run()
-    if err != nil {
-        var exitErr *exec.ExitError
-        if errors.As(err, &exitErr) {
-            r.exitCode = exitErr.ProcessState.ExitCode()
-            return &Error{
-                script: r.script,
-                command: r.command,
-                exitCode: r.exitCode,
-                err: fmt.Errorf("[golang-exec/runner/local/Run()] runner failed: %#w\n", err),
-            }
-        } else {
-            r.exitCode = -1
-            return &Error{
-                script: r.script,
-                command: r.command,
-                exitCode: r.exitCode,
-                err: fmt.Errorf("[golang-exec/runner/local/Run()] cannot execute runner: %#w\n", err),
-            }
-        }
-    }
+	err := r.cmd.Run()
+	if err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			r.exitCode = exitErr.ProcessState.ExitCode()
+			return &Error{
+				script:   r.script,
+				command:  r.command,
+				exitCode: r.exitCode,
+				err:      fmt.Errorf("[golang-exec/runner/local/Run()] runner failed: %#w\n", err),
+			}
+		} else {
+			r.exitCode = -1
+			return &Error{
+				script:   r.script,
+				command:  r.command,
+				exitCode: r.exitCode,
+				err:      fmt.Errorf("[golang-exec/runner/local/Run()] cannot execute runner: %#w\n", err),
+			}
+		}
+	}
 
-    return nil
+	return nil
 }
 
 func (r *Runner) Start() error {
-    err := r.cmd.Start()
-    if err != nil {
-        r.exitCode = -1
-        return &Error{
-            script: r.script,
-            command: r.command,
-            exitCode: r.exitCode,
-            err: fmt.Errorf("[golang-exec/runner/local/Start()] cannot start runner: %#w\n", err),
-        }
-    }
+	err := r.cmd.Start()
+	if err != nil {
+		r.exitCode = -1
+		return &Error{
+			script:   r.script,
+			command:  r.command,
+			exitCode: r.exitCode,
+			err:      fmt.Errorf("[golang-exec/runner/local/Start()] cannot start runner: %#w\n", err),
+		}
+	}
 
-    return nil
+	return nil
 }
 
 func (r *Runner) Wait() error {
-    err := r.cmd.Wait()
-    if err != nil {
-        var exitErr  *exec.ExitError
-        if errors.As(err, &exitErr) {
-            r.exitCode = exitErr.ProcessState.ExitCode()
-        } else {
-            r.exitCode = -1
-        }
-        return &Error{
-            script: r.script,
-            command: r.command,
-            exitCode: r.exitCode,
-            err: fmt.Errorf("[golang-exec/runner/local/Wait()] runner failed: %#w\n", err),
-        }
-    }
+	err := r.cmd.Wait()
+	if err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			r.exitCode = exitErr.ProcessState.ExitCode()
+		} else {
+			r.exitCode = -1
+		}
+		return &Error{
+			script:   r.script,
+			command:  r.command,
+			exitCode: r.exitCode,
+			err:      fmt.Errorf("[golang-exec/runner/local/Wait()] runner failed: %#w\n", err),
+		}
+	}
 
-    r.exitCode = 0
-    return nil
+	r.exitCode = 0
+	return nil
 }
 
 func (r *Runner) Close() error {
-    if r.cancel != nil {
-        r.cancel()
-    }
+	if r.cancel != nil {
+		r.cancel()
+	}
 
-    return nil
+	return nil
 }
 
 func (r *Runner) ExitCode() int {
-    return r.exitCode
+	return r.exitCode
 }
 
 //------------------------------------------------------------------------------
--- runner/ssh/runner.go
+++ runner/ssh/runner.go
@@ -7,298 +7,354 @@
 package ssh
 
 import (
-    "errors"
-    "fmt"
-    "github.com/mitchellh/go-homedir"
-    "io"
-    "golang.org/x/crypto/ssh/knownhosts"
-    "reflect"
-    "golang.org/x/crypto/ssh"
-    "strconv"
-    "strings"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
 
-    "github.com/stefaanc/golang-exec/script"
+	"github.com/mitchellh/go-homedir"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/knownhosts"
+	"golang.org/x/crypto/ssh/terminal"
+
+	"github.com/stefaanc/golang-exec/script"
 )
 
 //------------------------------------------------------------------------------
 
 type Connection struct {
-    Type     string   // must be "ssh"
-    Host     string
-    Port     uint16
-    User     string
-    Password string
-    Insecure bool
+	Type     string // must be "ssh"
+	Host     string
+	Port     uint16
+	User     string
+	Password string
+	Insecure bool
 }
 
 type Error struct {
-    script   *script.Script
-    command  string
-    exitCode int
-    err      error
+	script   *script.Script
+	command  string
+	exitCode int
+	err      error
 }
 
 type Runner struct {
-    script  *script.Script
-    command string
-    client  *ssh.Client
-    session *ssh.Session
-    running bool
+	script  *script.Script
+	command string
+	client  *ssh.Client
+	session *ssh.Session
+	running bool
 
-    exitCode int
+	exitCode int
 }
 
 //------------------------------------------------------------------------------
 
-func (e *Error) Script()   *script.Script { return e.script }
-func (e *Error) Command()  string         { return e.command }
-func (e *Error) ExitCode() int            { return e.exitCode }
-func (e *Error) Error()    string         { return e.err.Error() }
-func (e *Error) Unwrap()   error          { return e.err }
+func (e *Error) Script() *script.Script { return e.script }
+func (e *Error) Command() string        { return e.command }
+func (e *Error) ExitCode() int          { return e.exitCode }
+func (e *Error) Error() string          { return e.err.Error() }
+func (e *Error) Unwrap() error          { return e.err }
 
 //------------------------------------------------------------------------------
 
 func New(connection interface{}, s *script.Script, arguments interface{}) (*Runner, error) {
-    if s.Error != nil {
-        return nil, &Error{
-            script: s,
-            exitCode: -1,
-            err: fmt.Errorf("[golang-exec/runner/ssh/New()] script failed to parse: %#w\n", s.Error),
-        }
-    }
+	if s.Error != nil {
+		return nil, &Error{
+			script:   s,
+			exitCode: -1,
+			err:      fmt.Errorf("[golang-exec/runner/ssh/New()] script failed to parse: %#w\n", s.Error),
+		}
+	}
 
-    c := toConnection(connection)
-    r := new(Runner)
-    r.script = s
-    r.command = s.Command()
+	scriptReader, err := s.NewReader(arguments)
+	if err != nil {
+		return nil, &Error{
+			script:   s,
+			exitCode: -1,
+			err:      fmt.Errorf("[golang-exec/runner/ssh/New()] cannot create stdin reader: %#w\n", err),
+		}
+	}
+	var script string
+	if b, err := ioutil.ReadAll(scriptReader); err == nil {
+		script = string(b)
+	}
 
-    stdin, err := s.NewReader(arguments)
-    if err != nil {
-        return nil, &Error{
-            script: s,
-            exitCode: -1,
-            err: fmt.Errorf("[golang-exec/runner/ssh/New()] cannot create stdin reader: %#w\n", err),
-        }
-    }
+	c := toConnection(connection)
+	r := new(Runner)
+	r.script = s
+	r.command = s.Command() + script
 
-    address := fmt.Sprintf("%s:%d", c.Host, c.Port)
+	address := fmt.Sprintf("%s:%d", c.Host, c.Port)
 
-    config := &ssh.ClientConfig{
-        User: c.User,
-        Auth: []ssh.AuthMethod{
-            ssh.Password(c.Password),
-        },
-    }
-    if c.Insecure {
-        config.HostKeyCallback = ssh.InsecureIgnoreHostKey()
-    } else {
-        f, err := homedir.Expand("~/.ssh/known_hosts")
-        if err != nil {
-            return nil, &Error{
-                script: s,
-                exitCode: -1,
-                err: fmt.Errorf("[golang-exec/runner/ssh/New()] cannot find home directory of current user: %#w\n", err),
-            }
-        }
+	config := &ssh.ClientConfig{
+		User: c.User,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(c.Password),
+		},
+	}
+	if c.Insecure {
+		config.HostKeyCallback = ssh.InsecureIgnoreHostKey()
+	} else {
+		f, err := homedir.Expand("~/.ssh/known_hosts")
+		if err != nil {
+			return nil, &Error{
+				script:   s,
+				exitCode: -1,
+				err:      fmt.Errorf("[golang-exec/runner/ssh/New()] cannot find home directory of current user: %#w\n", err),
+			}
+		}
 
-        hostKeyCallback, err := knownhosts.New(f)
-        if err != nil {
-            return nil, &Error{
-                script: s,
-                exitCode: -1,
-                err: fmt.Errorf("[golang-exec/runner/ssh/New()] cannot access 'known_hosts'-file: %#w\n", err),
-            }
-        }
-        config.HostKeyCallback = hostKeyCallback
-    }
+		hostKeyCallback, err := knownhosts.New(f)
+		if err != nil {
+			return nil, &Error{
+				script:   s,
+				exitCode: -1,
+				err:      fmt.Errorf("[golang-exec/runner/ssh/New()] cannot access 'known_hosts'-file: %#w\n", err),
+			}
+		}
+		config.HostKeyCallback = hostKeyCallback
+	}
 
-    client, err := ssh.Dial("tcp", address, config)
-    if err != nil {
-        return nil, &Error{
-            script: s,
-            exitCode: -1,
-            err: fmt.Errorf("[golang-exec/runner/ssh/New()] cannot dial host: %#w\n", err),
-        }
-    }
-    r.client = client
+	client, err := ssh.Dial("tcp", address, config)
+	if err != nil {
+		return nil, &Error{
+			script:   s,
+			exitCode: -1,
+			err:      fmt.Errorf("[golang-exec/runner/ssh/New()] cannot dial host: %#w\n", err),
+		}
+	}
+	r.client = client
 
-    session, err := client.NewSession()
-    if err != nil {
-        return nil, &Error{
-            script: s,
-            exitCode: -1,
-            err: fmt.Errorf("[golang-exec/runner/ssh/New()] cannot open session: %#w\n", err),
-        }
-    }
-    r.session = session
-    r.session.Stdin  = stdin
+	session, err := client.NewSession()
+	if err != nil {
+		return nil, &Error{
+			script:   s,
+			exitCode: -1,
+			err:      fmt.Errorf("[golang-exec/runner/ssh/New()] cannot open session: %#w\n", err),
+		}
+	}
+	r.session = session
+	// r.session.Stdin = os.Stdin
 
-    return r, nil
+	return r, nil
 }
 
 func toConnection(connection interface{}) *Connection {
-    c := new(Connection)
+	c := new(Connection)
 
-    v := reflect.Indirect(reflect.ValueOf(connection))
-    if v.Kind() == reflect.Struct {
-        c.Type     = v.FieldByName("Type").String()
-        c.Host     = v.FieldByName("Host").String()
-        c.Port     = uint16( v.FieldByName("Port").Uint() )
-        c.User     = v.FieldByName("User").String()
-        c.Password = v.FieldByName("Password").String()
-        c.Insecure = v.FieldByName("Insecure").Bool()
-    } else if v.Kind() == reflect.Map {
-        iter := v.MapRange()
-        for iter.Next() {
-            switch iter.Key().String() {
-            case "Type":
-                c.Type     = iter.Value().String()
-            case "Host":
-                c.Host     = iter.Value().String()
-            case "Port":
-                p, err := strconv.ParseUint(iter.Value().String(), 10, 16)
-                if err != nil {
-                    p = 0
-                }
-                c.Port     = uint16(p)
-            case "User":
-                c.User     = iter.Value().String()
-            case "Password":
-                c.Password = iter.Value().String()
-            case "Insecure":
-                b, err := strconv.ParseBool(strings.ToLower(iter.Value().String()))
-                if err != nil {
-                    b = false
-                }
-                c.Insecure = b
-            }
-        }
-    }
+	v := reflect.Indirect(reflect.ValueOf(connection))
+	if v.Kind() == reflect.Struct {
+		c.Type = v.FieldByName("Type").String()
+		c.Host = v.FieldByName("Host").String()
+		c.Port = uint16(v.FieldByName("Port").Uint())
+		c.User = v.FieldByName("User").String()
+		c.Password = v.FieldByName("Password").String()
+		c.Insecure = v.FieldByName("Insecure").Bool()
+	} else if v.Kind() == reflect.Map {
+		iter := v.MapRange()
+		for iter.Next() {
+			switch iter.Key().String() {
+			case "Type":
+				c.Type = iter.Value().String()
+			case "Host":
+				c.Host = iter.Value().String()
+			case "Port":
+				p, err := strconv.ParseUint(iter.Value().String(), 10, 16)
+				if err != nil {
+					p = 0
+				}
+				c.Port = uint16(p)
+			case "User":
+				c.User = iter.Value().String()
+			case "Password":
+				c.Password = iter.Value().String()
+			case "Insecure":
+				b, err := strconv.ParseBool(strings.ToLower(iter.Value().String()))
+				if err != nil {
+					b = false
+				}
+				c.Insecure = b
+			}
+		}
+	}
 
-    return c
+	return c
 }
 
 //------------------------------------------------------------------------------
 
 func (r *Runner) SetStdoutWriter(stdout io.Writer) {
-    r.session.Stdout = stdout
+	r.session.Stdout = stdout
 }
 
 func (r *Runner) SetStderrWriter(stderr io.Writer) {
-    r.session.Stderr = stderr
+	r.session.Stderr = stderr
 }
 
 func (r *Runner) StdoutPipe() (io.Reader, error) {
-    reader, err := r.session.StdoutPipe()
-    if err != nil {
-        r.exitCode = -1
-        return nil, &Error{
-            script: r.script,
-            exitCode: r.exitCode,
-            err: fmt.Errorf("[golang-exec/runner/ssh/StdoutPipe()] cannot create stdout reader: %#w\n", err),
-        }
-    }
+	reader, err := r.session.StdoutPipe()
+	if err != nil {
+		r.exitCode = -1
+		return nil, &Error{
+			script:   r.script,
+			exitCode: r.exitCode,
+			err:      fmt.Errorf("[golang-exec/runner/ssh/StdoutPipe()] cannot create stdout reader: %#w\n", err),
+		}
+	}
 
-    return reader, nil
+	return reader, nil
 }
 
 func (r *Runner) StderrPipe() (io.Reader, error) {
-    reader, err := r.session.StderrPipe()
-    if err != nil {
-        r.exitCode = -1
-        return nil, &Error{
-            script: r.script,
-            exitCode: r.exitCode,
-            err: fmt.Errorf("[golang-exec/runner/ssh/StderrPipe()] cannot create stderr reader: %#w\n", err),
-        }
-    }
+	reader, err := r.session.StderrPipe()
+	if err != nil {
+		r.exitCode = -1
+		return nil, &Error{
+			script:   r.script,
+			exitCode: r.exitCode,
+			err:      fmt.Errorf("[golang-exec/runner/ssh/StderrPipe()] cannot create stderr reader: %#w\n", err),
+		}
+	}
 
-    return reader, nil
+	return reader, nil
 }
 
 func (r *Runner) Run() error {
-    err := r.session.Run(r.command)
-    if err != nil {
-        var exitErr *ssh.ExitError
-        if errors.As(err, &exitErr) {
-            r.exitCode = exitErr.Waitmsg.ExitStatus()
-            return &Error{
-                script: r.script,
-                command: r.command,
-                exitCode: r.exitCode,
-                err: fmt.Errorf("[golang-exec/runner/ssh/Run()] runner failed: %#w\n", err),
-            }
-        } else {
-            r.exitCode = -1
-            return &Error{
-                script: r.script,
-                command: r.command,
-                exitCode: r.exitCode,
-                err: fmt.Errorf("[golang-exec/runner/ssh/Run()] cannot execute runner: %#w\n", err),
-            }
-        }
-    }
+	modes := ssh.TerminalModes{
+		ssh.ECHOCTL:       0,
+		ssh.ECHO:          1,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
 
-    r.exitCode = 0
-    return nil
+	// stdin, err := r.session.StdinPipe()
+	// if err != nil {
+	// 	return fmt.Errorf("Unable to setup stdin for session: %v", err)
+	// }
+	// go io.Copy(stdin, os.Stdin)
+
+	// stdout, err := r.session.StdoutPipe()
+	// if err != nil {
+	// 	return fmt.Errorf("Unable to setup stdout for session: %v", err)
+	// }
+	// go io.Copy(os.Stdout, stdout)
+
+	// stderr, err := r.session.StderrPipe()
+	// if err != nil {
+	// 	return fmt.Errorf("Unable to setup stderr for session: %v", err)
+	// }
+	// go io.Copy(os.Stderr, stderr)
+
+	// r.session.Stdout = os.Stdout
+	// r.session.Stderr = os.Stderr
+	r.session.Stdin = os.Stdin
+	fileDescriptor := int(os.Stdin.Fd())
+	if terminal.IsTerminal(fileDescriptor) {
+		originalState, err := terminal.MakeRaw(fileDescriptor)
+		if err != nil {
+			return err
+		}
+		defer terminal.Restore(fileDescriptor, originalState)
+
+		termWidth, termHeight, err := terminal.GetSize(fileDescriptor)
+		if err != nil {
+			return err
+		}
+
+		err = r.session.RequestPty("xterm-256color", termHeight, termWidth, modes)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Printf("Running command: %v", r.command)
+	err := r.session.Run(r.command)
+	if err != nil {
+		var exitErr *ssh.ExitError
+		if errors.As(err, &exitErr) {
+			r.exitCode = exitErr.Waitmsg.ExitStatus()
+			return &Error{
+				script:   r.script,
+				command:  r.command,
+				exitCode: r.exitCode,
+				err:      fmt.Errorf("[golang-exec/runner/ssh/Run()] runner failed: %#w\n", err),
+			}
+		} else {
+			r.exitCode = -1
+			return &Error{
+				script:   r.script,
+				command:  r.command,
+				exitCode: r.exitCode,
+				err:      fmt.Errorf("[golang-exec/runner/ssh/Run()] cannot execute runner: %#w\n", err),
+			}
+		}
+	}
+
+	r.exitCode = 0
+	return nil
 }
 
 func (r *Runner) Start() error {
-    err := r.session.Start(r.command)
-    if err != nil {
-        r.exitCode = -1
-        return &Error{
-            script: r.script,
-            command: r.command,
-            exitCode: r.exitCode,
-            err: fmt.Errorf("[golang-exec/runner/ssh/Start()] cannot start runner: %#w\n", err),
-        }
-    }
-    r.running = true
+	err := r.session.Start(r.command)
+	if err != nil {
+		r.exitCode = -1
+		return &Error{
+			script:   r.script,
+			command:  r.command,
+			exitCode: r.exitCode,
+			err:      fmt.Errorf("[golang-exec/runner/ssh/Start()] cannot start runner: %#w\n", err),
+		}
+	}
+	r.running = true
 
-    return nil
+	return nil
 }
 
 func (r *Runner) Wait() error {
-    err := r.session.Wait()
-    r.running = false
-    if err != nil {
-        var exitErr *ssh.ExitError
-        if errors.As(err, &exitErr) {
-            r.exitCode = exitErr.Waitmsg.ExitStatus()
-        } else {
-            r.exitCode = -1
-        }
-        return &Error{
-            script: r.script,
-            command: r.command,
-            exitCode: r.exitCode,
-            err: fmt.Errorf("[golang-exec/runner/ssh/Wait()] runner failed: %#w\n", err),
-        }
-    }
+	err := r.session.Wait()
+	r.running = false
+	if err != nil {
+		var exitErr *ssh.ExitError
+		if errors.As(err, &exitErr) {
+			r.exitCode = exitErr.Waitmsg.ExitStatus()
+		} else {
+			r.exitCode = -1
+		}
+		return &Error{
+			script:   r.script,
+			command:  r.command,
+			exitCode: r.exitCode,
+			err:      fmt.Errorf("[golang-exec/runner/ssh/Wait()] runner failed: %#w\n", err),
+		}
+	}
 
-    r.exitCode = 0
-    return nil
+	r.exitCode = 0
+	return nil
 }
 
 func (r *Runner) Close() error {
-    if r.running {
-        _ = r.session.Signal(ssh.SIGTERM)
-    }
+	if r.running {
+		_ = r.session.Signal(ssh.SIGTERM)
+	}
 
-    if r.session != nil {
-        _ = r.session.Close()
-    }
+	if r.session != nil {
+		_ = r.session.Close()
+	}
 
-    if r.client != nil {
-        r.client.Close()
-    }
+	if r.client != nil {
+		r.client.Close()
+	}
 
-    return nil
+	return nil
 }
 
 func (r *Runner) ExitCode() int {
-    return r.exitCode
+	return r.exitCode
 }
 
 //------------------------------------------------------------------------------
